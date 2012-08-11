bf67f5f01f81a370b7f142a76ad5d03afa4c4fcb
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- modules/psychtest/language/english/modinfo.php
+++ modules/psychtest/language/english/modinfo.php
@@ -1,14 +1,14 @@
 <?php
 
-	define('_XS_BLOBS', 'Number of blobs to use in ink blotter');
-	define('_XS_BLOBS_DESC', 'This is the number of blobs used in the ink blotter.');
-	define('_XS_WIDTH', 'Blotter Width');
-	define('_XS_WIDTH_DESC', 'This is the width of the image');
-	define('_XS_HEIGHT', 'Blotter Height');
-	define('_XS_HEIGHT_DESC', 'This is the height of the image');			
-	define('_XS_BASETRANSPARENCY', 'Base Transparency');	
-	define('_XS_BASETRANSPARENCY_DESC', 'This is the lowest level of transparency to use with blob in a random range.');	
-	define('_XS_UPPERTRANSPARENCY', 'Upper Transparency');	
-	define('_XS_UPPERTRANSPARENCY_DESC', 'This is the hightest level of transparency to use with blob in a random range.');				
+	define('_XS_BLOBS','Number of blobs to use in ink blotter');
+	define('_XS_BLOBS_DESC','This is the number of blobs used in the ink blotter.');
+	define('_XS_WIDTH','Blotter Width');
+	define('_XS_WIDTH_DESC','This is the width of the image');
+	define('_XS_HEIGHT','Blotter Height');
+	define('_XS_HEIGHT_DESC','This is the height of the image');			
+	define('_XS_BASETRANSPARENCY','Base Transparency');	
+	define('_XS_BASETRANSPARENCY_DESC','This is the lowest level of transparency to use with blob in a random range.');	
+	define('_XS_UPPERTRANSPARENCY','Upper Transparency');	
+	define('_XS_UPPERTRANSPARENCY_DESC','This is the hightest level of transparency to use with blob in a random range.');				
 	
 ?>
